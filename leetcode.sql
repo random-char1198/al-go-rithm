b908f2ec3f68b659897ef5eec8c5a3dba9683311
Payton Dong Pei <silence1198@outlook.com>
2021-05-25 23:23:45 -0400

    182

--- leetcode.sql
+++ leetcode.sql
@@ -1,7 +1,3 @@
-
-SELECT max(salary) AS SecondHighestSalary 
-FROM employee 
-WHERE salary < (SELECT max(salary) FROM employee)
 -- +----+--------+
 -- | Id | Salary |
 -- +----+--------+
@@ -13,5 +9,31 @@ WHERE salary < (SELECT max(salary) FROM employee)
 -- Inner query will be executed first, therefore
 -- $300 will be returned. Outer query will also return highest salary but it will be less than the inner loop.
 -- SO $200 will be returned
+SELECT max(salary) AS SecondHighestSalary 
+FROM employee 
+WHERE salary < (SELECT max(salary) FROM employee)
 
+
+
+--182
+--remove duplicates
+create table Test(id integer, email varchar(100));
+insert into Test(id, email) values(4, "test1@outlook.com");
+insert into Test(id, email) values(2, "test2@gmail.com");
+insert into Test(id, email) values(3, "test2@gmail.com");
+-- Test1
+select email
+from Test
+group by email
+-- will return 
+--email
+--dongpei1198@outlook.com
+--dongpei1198@gmail.com
+
+select email from student
+group by email
+having count(email)>1
+-- will return any entries that is more than 1.
+--email
+--dongpei1198@gmail.com
 
