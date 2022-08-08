0fa70f2f134a9a469200b691d7f60c8a2611bd09
mrspoon119 <paytonpei@cmail.carleton.ca>
2022-08-08 17:10:53 -0400

    happy_number_202.py

---
+++ leetcode/no/happy_number_202.py
@@ -0,0 +1,16 @@
+class Solution:
+    def isHappy(self, n: int) -> bool:
+        s = set()
+        while n not in s:
+            s.add(n)
+            tmp = 0
+            for i in str(n):
+                tmp += int(i) ** 2
+            if tmp == 1:
+                return True
+            else:
+                n = tmp
+        return False
+
+solution = Solution()
+print(solution.isHappy(2))
