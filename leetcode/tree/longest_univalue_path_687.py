391a3d25ab3274d694c74e7c9b72452314e08b3e
mrspoon119 <paytonpei@cmail.carleton.ca>
2022-08-15 15:48:13 -0400

    longest_univalue_path_687.pys

--- leetcode/tree/longest_univalue_path_687.py
+++ leetcode/tree/longest_univalue_path_687.py
@@ -2,7 +2,7 @@ class Solution:
     def __init__(self):
         self.max_path = 0
 
-    def longestUnivaluePath(self, root:TreeNode) -> int:
+    def longestUnivaluePath(self, root: TreeNode) -> int:
         def dfs(root):
             if not root:
                 return 0
@@ -14,8 +14,8 @@ class Solution:
             if root.right:
                 right = right + 1 if root.right.val == root.val else 0
 
-
             self.max_path = max(self.max_path, left + right)
             return max(left, right)
+
         dfs(root)
         return self.max_path
