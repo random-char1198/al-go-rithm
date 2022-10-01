52418259f3ae847c8cac9776f8bcb4a2c2c8a3bf
P A I <silence1198@outlook.com>
2022-10-01 16:06:07 -0400

    update notebook and complete set_mismatch_645.py

--- leetcode/set/set_mismatch_645.py
+++ leetcode/set/set_mismatch_645.py
@@ -1,11 +1,17 @@
+from collections import defaultdict
 class Solution:
-    def findErrorNums(self, nums: list[int]) -> list[int]:
-        res = []
-        for i in range(1, len(nums) - 1, 2):
-            if nums[i] == nums[i - 1]:
-                res += nums[i]
+    def findErrorNums(self, nums: List[int]) -> List[int]:
+        dic = defaultdict()
+        res = [-1] * 2
+        for i in range(len(nums)):
+            if nums[i] not in dic:
+                dic[nums[i]] = 1
+            else:
+                dic[nums[i]] += 1
+        # print(dic)
+        for i in range(1,len(nums)+1,1):
+            if i not in dic:
+                res[1] = i
+            elif dic[i] == 2:
+                res[0] = i
         return res
-
-
-s = Solution()
-print(s.findErrorNums([1, 2, 2, 4]))
