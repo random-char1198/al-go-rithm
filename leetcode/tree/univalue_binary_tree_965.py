4a5f60ca7d26c11c8991b490e81163e037205cc4
mrspoon119 <paytonpei@cmail.carleton.ca>
2022-10-01 13:54:30 -0400

    add univalue_binary_tree_965.py

---
+++ leetcode/tree/univalue_binary_tree_965.py
@@ -0,0 +1,23 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def __init__(self):
+        self.s = set()
+
+    def dfs(self, node):
+        if not node:
+            return
+        else:
+            if node.val not in self.s:
+                # if not in s, add to set
+                self.s.add(node.val)
+            self.dfs(node.left)
+            self.dfs(node.right)
+
+    def isUnivalTree(self, root) -> bool:
+        self.dfs(root)
+        return len(self.s) == 1
