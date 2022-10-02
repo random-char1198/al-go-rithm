a96107f22a4f4c13642dc50c3375df7ec12d8b99
mrspoon119 <paytonpei@cmail.carleton.ca>
2022-10-02 16:01:19 -0400

    add oct_2_2022.py

---
+++ DailyCodeChallenge/oct_2_2022.py
@@ -0,0 +1,8 @@
+class Solution:
+    def checkOnesSegment(self, s: str) -> bool:
+        pointer = 0
+        while pointer < len(s) and s[pointer] == '1':
+            pointer += 1
+        while pointer < len(s) and s[pointer] == '0':
+            pointer += 1
+        return pointer == len(s)
