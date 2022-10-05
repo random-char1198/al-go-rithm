ea32c323a3ae8e5d22ea6caeea0a6ca4349a18f3
mrspoon119 <paytonpei@cmail.carleton.ca>
2022-10-05 17:43:42 -0400

    update PEP 8: E231

--- DailyCodeChallenge/oct_01_2022.py
+++ DailyCodeChallenge/oct_01_2022.py
@@ -1,23 +1,23 @@
 class Solution:
     def reformatNumber(self, number: str) -> str:
-        number = number.replace("-","").replace(" ",'')
+        number = number.replace("-", "").replace(" ", '')
         res = ""
-        for i in range(0,len(number),3):
+        for i in range(0, len(number), 3):
             if len(number[i:]) <= 4:
                 if len(number[i:]) == 4:
-                    res += number[i:i+2]
+                    res += number[i:i + 2]
                     res += "-"
-                    res += number[i+2:]
+                    res += number[i + 2:]
                 elif len(number[i:]) == 3:
-                    res += number[i:i+3]
+                    res += number[i:i + 3]
                 elif len(number[i:]) == 2:
-                    res += number[i:i+2]
+                    res += number[i:i + 2]
                 else:
                     print("weird happened")
             else:
                 if i + 3 == len(number):
-                    res += number[i:i+3]
+                    res += number[i:i + 3]
                 else:
-                    res += number[i:i+3]
+                    res += number[i:i + 3]
                     res += '-'
         return res
