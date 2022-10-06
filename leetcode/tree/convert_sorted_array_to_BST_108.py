7f1cafec9c865e3015dd0bee5b77015c480eac2b
mrspoon119 <paytonpei@cmail.carleton.ca>
2022-10-05 21:10:54 -0400

    add convert_sorted_array_to_BST_108.py

---
+++ leetcode/tree/convert_sorted_array_to_BST_108.py
@@ -0,0 +1,20 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def sortedArrayToBST(self, nums: List[int]) -> Optional[TreeNode]:
+        # int mid = l + (r-l)/2。
+        def dfs(left, right):
+            if left > right:
+                return None
+
+            mid = (left + right + 1) // 2
+            root = TreeNode(nums[mid])
+            root.left = dfs(left, mid - 1)
+            root.right = dfs(mid + 1, right)
+            return root
+
+        return dfs(0, len(nums) - 1)
